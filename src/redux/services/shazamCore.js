cec664adfd9a456c71c013c67540ac693a5afdd6
Frank South <frank@franksouth.dev>
2024-04-07 19:42:00 -0400

    Changing Discover component to dynamically update, rather than using the placeholder static data

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '04d02494afmsh857cfc815ac2243p1792b0jsnd66a2f5b9915',
-      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '04d02494afmsh857cfc815ac2243p1792b0jsnd66a2f5b9915');
 
-    export const shazamCoreApi = createApi({
-      reducerPath: 'shazamCoreApi',
-    })
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
