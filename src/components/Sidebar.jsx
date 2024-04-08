984866d981e8cadcb0811789a27f1e1eeb918f35
Frank South <frank@franksouth.dev>
2024-04-08 12:20:16 -0400

    Details and search functionality, updates to shazamCore

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -38,7 +38,6 @@ const Sidebar = () => {
         <NavLinks />
       </div>
 
-      {/* Mobile sidebar */}
       <div className="absolute md:hidden block top-6 right-3">
         {!mobileMenuOpen ? (
           <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
